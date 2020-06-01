90e97c3cfae2ff705c3f538004a7bd7d31031477
Callum Wilsmore <cal.wilsmore@btinternet.com>
2020-06-01 19:21:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function yourAnimal() {
 
 function add2(n) {
   const two = 2
-
+  
   return n + two
 
   // Feel free to move things around!
-
+  
 }
 
 var funkyFunction = function() {
@@ -33,4 +33,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var funkyFunction = theFunk
+var theFunk = funkyFunction
